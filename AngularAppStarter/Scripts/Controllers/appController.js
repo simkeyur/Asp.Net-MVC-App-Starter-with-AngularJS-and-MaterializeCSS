3dacb22bcee6168e096219475bd840314b302690
Keyur Patel <simkeyur@gmail.com>
2018-01-27 02:36:22 -0600

    Minor Change

--- AngularAppStarter/Scripts/Controllers/appController.js
+++ AngularAppStarter/Scripts/Controllers/appController.js
@@ -1,5 +1,5 @@
 ﻿var myApp = angular.module('myApp', []);
 
 myApp.controller('appController', ['$scope', function ($scope) {
-    $scope.greeting = 'Hola!';
+    $scope.greeting = 'Hi There!';
 }]);
